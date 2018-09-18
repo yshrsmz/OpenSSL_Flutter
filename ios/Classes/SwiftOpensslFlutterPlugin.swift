7789b70991ca16f5a575fca739c18c92d9d2112c
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-09-18 23:08:30 +0900

    call openssl from swift

--- ios/Classes/SwiftOpensslFlutterPlugin.swift
+++ ios/Classes/SwiftOpensslFlutterPlugin.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import openssl
+import os.log
     
 public class SwiftOpensslFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,10 +11,43 @@ public class SwiftOpensslFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "getSha512Digest":
+        result(getSha512Digest())
+    default:
+        result("iOS " + UIDevice.current.systemVersion)
+    }
   }
     
-    func test(){
+    func getSha512Digest() -> String {
+        var ctxp = RIPEMD160_CTX()
+        let data = "test".cString(using: .utf8)
+        var result = [UInt8](repeating: 0, count: Int(RIPEMD160_DIGEST_LENGTH))
         
+        RIPEMD160_Init(&ctxp)
+        RIPEMD160_Update(&ctxp, data, data!.count - 1)
+        RIPEMD160_Final(&result, &ctxp)
+        
+        return result.hexa
+    }
+}
+
+extension String {
+    var hexaBytes: [UInt8] {
+        var position = startIndex
+        return (0..<count/2).compactMap { _ in
+            defer { position = index(position, offsetBy: 2) }
+            return UInt8(self[position...index(after: position)], radix: 16)
+        }
+    }
+    var hexaData: Data { return hexaBytes.data }
+}
+
+extension Collection where Iterator.Element == UInt8 {
+    var data: Data {
+        return Data(self)
+    }
+    var hexa: String {
+        return map{ String(format: "%02X", $0) }.joined()
     }
 }
--- ios/openssl_flutter.podspec
+++ ios/openssl_flutter.podspec
@@ -14,18 +14,10 @@ A new flutter plugin project.
   s.source           = { :path => '.' }
   
   s.source_files = 'Classes/**/*'
-  s.header_mappings_dir = 'Classes'
-  s.public_header_files = 'Classes/**/*.h'
-  s.libraries = 'crypto', 'ssl'
-  s.vendored_libraries = 'OpenSSL/lib/libssl.a', 'OpenSSL/lib/libcrypto.a'
-  s.preserve_paths = 'OpenSSL/module/module.modulemap', 'OpenSSL/include/openssl/*.h'
+  s.vendored_frameworks = 'OpenSSL/openssl.framework'
+  
   s.dependency 'Flutter'
   
-  s.pod_target_xcconfig = {
-      'SWIFT_INCLUDE_PATHS' => '$(PODS_ROOT)/openssl_flutter/OpenSSL/module $(PODS_TARGET_ROOT)/OpenSSL/module',
-      #'HEADER_SEARCH_PATHS' => '$(PODS_ROOT)/openssl_flutter/OpenSSL/include/openssl $(PODS_TARGET_ROOT)/OpenSSL/include/openssl',
-      'LIBRARY_SEARCH_PATHS' => '$(PODS_ROOT)/openssl_flutter/OpenSSL/lib  $(PODS_TARGET_ROOT)/OpenSSL/lib'
-  }
   
   s.ios.deployment_target = '8.0'
 end
